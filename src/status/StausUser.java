720807a5f13a271b70ea7d8f6a84f187e965313c
ShreyGondhiya <gondhiya@sheridancollege.ca>
2024-10-16 18:16:55 -0400

    push

--- src/status/StausUser.java
+++ src/status/StausUser.java
@@ -9,26 +9,23 @@ package status;
  * MidTerm-Exam
  * @author srinivsi
  */
-public class StausUser 
-{
-   public void statusDetail(String code)
-{
-switch(code.toUpperCase())
-        {
-        case "ZERO": System.out.println("REJECTED");
-        break;
-        case "ONE": System.out.println("PENDING");
-        break;
-        case "TWO":
-        System.out.println("PROCESSING");
-        break;
-        case "THREE": 
-            System.out.println("APPROVED");
-        break;
-        default:
-         System.out.println("NOT VALID CODE");
-        break;
+public class StausUser {
+    
+    public void statusDetail(UserStatus status) {
+        switch (status) {
+            case ZERO:
+                System.out.println("User status: ZERO");
+                break;
+            case ONE:
+                System.out.println("User status: ONE");
+                break;
+            case TWO:
+                System.out.println("User status: TWO");
+                break;
+            case THREE:
+                System.out.println("User status: THREE");
+                break;
         }
+    }
 }
- 
-}
+
